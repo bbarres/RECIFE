16edcd7d493902943daf5db010d1167e0e0d89b8
bbarres <benoit.barres@gmail.com>
2019-05-06 15:56:31 +0200

    correcting some glitches and adding a simple graph for the results

--- recif_mycGrowth.R
+++ recif_mycGrowth.R
@@ -20,7 +20,7 @@ datamyc<-read.table("data/cerco_mars19.txt",header=TRUE,sep=";")
 #first we extract the list of the different SA listed in the file
 SAlist<-levels(datamyc$pest_sa_id)
 CompRez<-data.frame(Subs_Act=factor(),sample_ID=factor(),
-                    ED50=numeric())
+                    ED50=character())
 #we make a subselection of the data according to the SA
 for (j in 1:length(SAlist)) {
   data_subSA<-datamyc[datamyc$pest_sa_id==SAlist[j],]
@@ -32,9 +32,9 @@ for (j in 1:length(SAlist)) {
                                   "ech_id"])
   ifelse(length(SA_rez)==0,
          REZSA<-data.frame(Subs_Act=factor(),sample_ID=factor(),
-                           ED50=numeric()),
+                           ED50=character()),
          REZSA<-data.frame("Subs_Act"=SAlist[j],"sample_ID"=SA_rez,
-                           "ED50"=max(data_subSA$dose)))
+                           "ED50"=paste(">",max(data_subSA$dose),sep="")))
   #we limit the dataset to the sample that reach somehow a IC of 50%
   SA.dat<-data_subSA[!(data_subSA$ech_id %in% SA_rez),]
   SA.dat<-drop.levels(SA.dat)
@@ -47,12 +47,24 @@ for (j in 1:length(SAlist)) {
     temp<-ED(temp.m1,50,type="absolute")
     tempx<-data.frame("Subs_Act"=SAlist[j],
                       "sample_ID"=names(table(SA.dat$ech_id))[i],
-                      "ED50"=temp[1])
+                      "ED50"=as.character(temp[1]))
     REZSA<-rbind(REZSA,tempx)
   }
   CompRez<-rbind(CompRez,REZSA)
 
 }
+
+#exporting the result as a text file
+write.table(CompRez, file="output/results_cerco.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
+
+#just a small graphic to gain insight on the first round of results
+#first, we replace the ED50 that were too high to be evaluated with the dose 
+#range used with an absurdly high value
+CompRez$ED50<-as.numeric(as.character(CompRez$ED50))
+CompRez[is.na(CompRez$ED50),"ED50"]<-10000
+barplot(as.numeric(as.character(CompRez$ED50)),
+        ylim=c(0,50),col=CompRez$Subs_Act)
 
 
 ###############################################################################
