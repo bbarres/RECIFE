de82e0a42a1b674439e8eb3afd463db1bd1e6cb6
bbarres <benoit.barres@gmail.com>
2025-09-15 18:57:21 +0200

    starting to add LD computation

--- recif_anaGenet.R
+++ recif_anaGenet.R
@@ -128,5 +128,57 @@ pairwise.WCfst(JDDade)
 
 
 ##############################################################################/
+#Linkage disequilibrium analyses####
+##############################################################################/
+
+coloor<-c("firebrick","royalblue4","chartreuse4","khaki2","darkorange")
+JDDmicroCCgenp<-genind2genpop(JDDmicroCC)
+
+onebyhost<-"data/onebyhost.txt"
+test_LD(onebyhost,"output/onebyhost.txt.DIS")
+clean_workdir()
+
+#the output file have to be edited so it can be used for representation
+LDbyhost<-readLines("output/onebyhost.txt.DIS")
+#removing the first lines that are non relevant
+LDcolnames<-unlist(strsplit(LDbyhost[13],"\\s+"))
+LDbyhost<-LDbyhost[-c(1:14)]
+#for 14 markers and 4 populations there are 
+14*13/2*4 #LD tests for markers pairs
+LDbyhost<-LDbyhost[-c(365:length(LDbyhost))]
+LDbyhost<-as.data.table(matrix(unlist(strsplit(LDbyhost,"\\s+")),
+                               nrow=length(strsplit(LDbyhost,"\\s+")),
+                               byrow=TRUE))
+colnames(LDbyhost)<-LDcolnames
+LDbyhost$`P-Value`<-as.numeric(LDbyhost$`P-Value`)
+
+#Linkage disequilibrium in the peach population
+LDpeach<-as.matrix(LDbyhost[c(1:91),])
+row.names(LDpeach)<-paste(LDpeach[,2],LDpeach[,3])
+LDpeach<-rbind(LDpeach[,c(2,3,4)],LDpeach[,c(3,2,4)])
+LDpeach<-spread(data.frame(LDpeach),2,3,fill="NA",convert=TRUE)
+#changing the rownames
+row.names(LDpeach)<-as.character(LDpeach[,1])
+#removing the first unnecessary column
+LDpeach<-LDpeach[,c(-1)]
+#reordering the columns and turning the object into a matrix
+LDpeach<-as.matrix(LDpeach[c(2,8,12,13,14,1,3:7,9:11),
+                           c(2,8,12,13,14,1,3:7,9:11)])
+
+#Linkage disequilibrium in the oil seed population
+LDoils<-as.matrix(LDbyhost[c(92:182),])
+row.names(LDoils)<-paste(LDoils[,2],LDoils[,3])
+LDoils<-rbind(LDoils[,c(2,3,4)],LDoils[,c(3,2,4)])
+LDoils<-spread(data.frame(LDoils),2,3,fill="NA",convert=TRUE)
+#changing the rownames
+row.names(LDoils)<-as.character(LDoils[,1])
+#removing the first unnecessary column
+LDoils<-LDoils[,c(-1)]
+#reordering the columns and turning the object into a matrix
+LDoils<-as.matrix(LDoils[c(2,8,12,13,14,1,3:7,9:11),
+                         c(2,8,12,13,14,1,3:7,9:11)])
+
+
+##############################################################################/
 #END
 ##############################################################################/
